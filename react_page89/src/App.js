0be1801ba9d2dffbed0e686d771103f33a33b019
kjg <o_opop9999@naver.com>
2023-06-02 18:06:52 +0900

    React 기초 0p-p89

---
+++ react_page89/src/App.js
@@ -0,0 +1,98 @@
+import './App.css';
+import {useState} from 'react';
+
+function Header(props){
+  return(
+    <header>
+      <h1><a href="/" onClick={function(event){
+        event.preventDefault();
+        props.onChangeMode();
+      }}>{props.title}</a></h1>
+    </header>
+  )  
+}
+
+function Nav(props){
+  const lis = []
+  for(let i=0; i<props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a id={t.id} href={'/read/'+t.id} onClick={event=>{
+      event.preventDefault();
+      props.onChangeMode (Number(event.target.id));
+    }}>{t.title}</a></li>);
+  }
+  return(
+    <nav>
+      <ol>
+        {lis}
+      </ol>
+    </nav>
+  )
+}
+
+function Article(props){
+  return(
+    <article>
+      <h2>{props.title}</h2>
+        {props.body}
+    </article>
+  )
+}
+
+function Create() {
+  return (
+    <article>
+      <h2>Create</h2>
+      <form>
+        <p><input type="text" name="title" placeholder="title" /></p>
+        <p> <textarea name="body" placeholder="body"></textarea></p>
+        <p><input type="submit" value="Create"></input></p>
+      </form>
+    </article>
+  )
+}
+
+function App() {
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+//  console.log('_mode', _mode);
+  const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'javascript', body:'javascript is ...'}
+  ]
+  let content = null;
+  if(mode ==='WELCOME'){
+    content = <Article title="Welcome" body="hello, Web"></Article>      
+  }else if(mode === 'READ'){
+    let title, body = null;
+    for(let i=0; i<topics.length; i++) {
+      console.log(topics[i].id, id);
+      if(topics[i].id === id) {
+      title = topics[i].title;
+      body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>      
+  }else if(mode === 'CREATE'){
+    content = <Create></Create>
+  }
+  return (
+    <div>
+      <Header title="WEB" onChangeMode={()=>{
+          setMode('WELCOME');
+      }}></Header>
+       <Nav topics = {topics} onChangeMode={(_id)=>{
+          setMode('READ');
+          setId(_id);
+       }}></Nav>
+      {content}
+      <a href="/create" onClick={event=>{
+        event.preventDefault();
+        setMode('CREATE');
+    }}>Create</a>
+    </div>
+  );
+}
+
+export default App;
